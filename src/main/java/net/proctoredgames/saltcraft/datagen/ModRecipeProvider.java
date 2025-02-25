a86e19bfe38829fb6449139a7896baece914ff9a
Proctored Games <proctoredgames@hotmail.com>
2025-02-24 20:44:00 -0800

    crafting recipes for all new food items

--- src/main/java/net/proctoredgames/saltcraft/datagen/ModRecipeProvider.java
+++ src/main/java/net/proctoredgames/saltcraft/datagen/ModRecipeProvider.java
@@ -1,10 +1,12 @@
 package net.proctoredgames.saltcraft.datagen;
 
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.StonecutterRecipe;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.proctoredgames.saltcraft.Saltcraft;
 import net.proctoredgames.saltcraft.block.ModBlocks;
 import net.proctoredgames.saltcraft.item.ModItems;
@@ -37,8 +39,48 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         oreBlasting(pWriter, SALT_ORE_SMELTABLES, RecipeCategory.MISC, ModItems.SALT.get(), 0.25f, 200, "salt");
         oreBlasting(pWriter, PINK_SALT_ORE_SMELTABLES, RecipeCategory.MISC, ModItems.PINK_SALT.get(), 0.25f, 200, "pink_salt");
 
-        SimpleCookingRecipeBuilder.smelting(Ingredient.of(new ItemLike[]{ModItems.SALTED_POTATO.get()}), RecipeCategory.FOOD, ModItems.SALTED_BAKED_POTATO.get(), 0.35F, 200).unlockedBy("has_salted_potato", has(ModItems.SALTED_POTATO.get())).save(pWriter, Saltcraft.MOD_ID + ":" + "salted_baked_potato_from_smelting_salted_potato");
-        SimpleCookingRecipeBuilder.smelting(Ingredient.of(new ItemLike[]{ModItems.PINK_SALTED_POTATO.get()}), RecipeCategory.FOOD, ModItems.PINK_SALTED_BAKED_POTATO.get(), 0.35F, 200).unlockedBy("has_pink_salted_potato", has(ModItems.PINK_SALTED_POTATO.get())).save(pWriter, Saltcraft.MOD_ID + ":" + "pink_salted_baked_potato_from_smelting_baked_salted_potato");
+        addSmeltingPair(
+                ModItems.SALTED_POTATO.get(), ModItems.SALTED_BAKED_POTATO.get(),
+                ModItems.PINK_SALTED_POTATO.get(), ModItems.PINK_SALTED_BAKED_POTATO.get(),
+                pWriter
+        );
+
+// Meat & Fish Smelting Pairs
+        addSmeltingPair(
+                ModItems.SALTED_BEEF.get(), ModItems.SALTED_COOKED_BEEF.get(),
+                ModItems.PINK_SALTED_BEEF.get(), ModItems.PINK_SALTED_COOKED_BEEF.get(),
+                pWriter
+        );
+        addSmeltingPair(
+                ModItems.SALTED_CHICKEN.get(), ModItems.SALTED_COOKED_CHICKEN.get(),
+                ModItems.PINK_SALTED_CHICKEN.get(), ModItems.PINK_SALTED_COOKED_CHICKEN.get(),
+                pWriter
+        );
+        addSmeltingPair(
+                ModItems.SALTED_COD.get(), ModItems.SALTED_COOKED_COD.get(),
+                ModItems.PINK_SALTED_COD.get(), ModItems.PINK_SALTED_COOKED_COD.get(),
+                pWriter
+        );
+        addSmeltingPair(
+                ModItems.SALTED_MUTTON.get(), ModItems.SALTED_COOKED_MUTTON.get(),
+                ModItems.PINK_SALTED_MUTTON.get(), ModItems.PINK_SALTED_COOKED_MUTTON.get(),
+                pWriter
+        );
+        addSmeltingPair(
+                ModItems.SALTED_PORKCHOP.get(), ModItems.SALTED_COOKED_PORKCHOP.get(),
+                ModItems.PINK_SALTED_PORKCHOP.get(), ModItems.PINK_SALTED_COOKED_PORKCHOP.get(),
+                pWriter
+        );
+        addSmeltingPair(
+                ModItems.SALTED_RABBIT.get(), ModItems.SALTED_COOKED_RABBIT.get(),
+                ModItems.PINK_SALTED_RABBIT.get(), ModItems.PINK_SALTED_COOKED_RABBIT.get(),
+                pWriter
+        );
+        addSmeltingPair(
+                ModItems.SALTED_SALMON.get(), ModItems.SALTED_COOKED_SALMON.get(),
+                ModItems.PINK_SALTED_SALMON.get(), ModItems.PINK_SALTED_COOKED_SALMON.get(),
+                pWriter
+        );
 
         stonecutterResultFromBase(pWriter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.CLUMPED_SALT_SLAB.get(), ModBlocks.CLUMPED_SALT_BLOCK.get(), 2);
         stonecutterResultFromBase(pWriter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.CLUMPED_SALT_STAIRS.get(), ModBlocks.CLUMPED_SALT_BLOCK.get());
@@ -306,37 +348,38 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .unlockedBy(getHasName(ModBlocks.ROCK_PINK_SALT_BLOCK.get()), has(ModBlocks.ROCK_PINK_SALT_BLOCK.get()))
                 .save(pWriter);
 
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.SALTED_POTATO.get())
-                .requires(ModItems.SALT.get())
-                .requires(Items.POTATO)
-                .unlockedBy(getHasName(ModItems.SALT.get()), has(ModItems.SALT.get()))
-                .unlockedBy(getHasName(Items.POTATO), has(Items.POTATO))
-                .save(pWriter);
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.SALTED_BAKED_POTATO.get())
-                .requires(ModItems.SALT.get())
-                .requires(Items.BAKED_POTATO)
-                .unlockedBy(getHasName(ModItems.SALT.get()), has(ModItems.SALT.get()))
-                .unlockedBy(getHasName(Items.BAKED_POTATO), has(Items.BAKED_POTATO))
-                .save(pWriter);
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.PINK_SALTED_POTATO.get())
-                .requires(ModItems.PINK_SALT.get())
-                .requires(Items.POTATO)
-                .unlockedBy(getHasName(ModItems.PINK_SALT.get()), has(ModItems.PINK_SALT.get()))
-                .unlockedBy(getHasName(Items.POTATO), has(Items.POTATO))
-                .save(pWriter);
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.PINK_SALTED_BAKED_POTATO.get())
-                .requires(ModItems.PINK_SALT.get())
-                .requires(Items.BAKED_POTATO)
-                .unlockedBy(getHasName(ModItems.PINK_SALT.get()), has(ModItems.PINK_SALT.get()))
-                .unlockedBy(getHasName(Items.BAKED_POTATO), has(Items.BAKED_POTATO))
-                .save(pWriter);
-
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Items.POTATO)
                 .requires(ModTags.Items.SALT_OR_PINK_SALT)
                 .requires(Items.POISONOUS_POTATO)
                 .unlockedBy(getHasName(ModItems.SALT.get()), has(ModItems.SALT.get()))
                 .unlockedBy(getHasName(Items.POISONOUS_POTATO), has(Items.POISONOUS_POTATO))
                 .save(pWriter);
+
+// Vegetables
+        addRecipePair(Items.POTATO, ModItems.SALTED_POTATO.get(), ModItems.PINK_SALTED_POTATO.get(), pWriter);
+        addRecipePair(Items.BAKED_POTATO, ModItems.SALTED_BAKED_POTATO.get(), ModItems.PINK_SALTED_BAKED_POTATO.get(), pWriter);
+        addRecipePair(Items.BEETROOT, ModItems.SALTED_BEETROOT.get(), ModItems.PINK_SALTED_BEETROOT.get(), pWriter);
+        addRecipePair(Items.CARROT, ModItems.SALTED_CARROT.get(), ModItems.PINK_SALTED_CARROT.get(), pWriter);
+
+// Raw Meats & Fish
+        addRecipePair(Items.BEEF, ModItems.SALTED_BEEF.get(), ModItems.PINK_SALTED_BEEF.get(), pWriter);
+        addRecipePair(Items.CHICKEN, ModItems.SALTED_CHICKEN.get(), ModItems.PINK_SALTED_CHICKEN.get(), pWriter);
+        addRecipePair(Items.COD, ModItems.SALTED_COD.get(), ModItems.PINK_SALTED_COD.get(), pWriter);
+        addRecipePair(Items.MUTTON, ModItems.SALTED_MUTTON.get(), ModItems.PINK_SALTED_MUTTON.get(), pWriter);
+        addRecipePair(Items.PORKCHOP, ModItems.SALTED_PORKCHOP.get(), ModItems.PINK_SALTED_PORKCHOP.get(), pWriter);
+        addRecipePair(Items.RABBIT, ModItems.SALTED_RABBIT.get(), ModItems.PINK_SALTED_RABBIT.get(), pWriter);
+        addRecipePair(Items.SALMON, ModItems.SALTED_SALMON.get(), ModItems.PINK_SALTED_SALMON.get(), pWriter);
+        addRecipePair(Items.PUFFERFISH, ModItems.SALTED_PUFFERFISH.get(), ModItems.PINK_SALTED_PUFFERFISH.get(), pWriter);
+        addRecipePair(Items.TROPICAL_FISH, ModItems.SALTED_TROPICAL_FISH.get(), ModItems.PINK_SALTED_TROPICAL_FISH.get(), pWriter);
+
+// Cooked Meats & Fish
+        addRecipePair(Items.COOKED_BEEF, ModItems.SALTED_COOKED_BEEF.get(), ModItems.PINK_SALTED_COOKED_BEEF.get(), pWriter);
+        addRecipePair(Items.COOKED_CHICKEN, ModItems.SALTED_COOKED_CHICKEN.get(), ModItems.PINK_SALTED_COOKED_CHICKEN.get(), pWriter);
+        addRecipePair(Items.COOKED_COD, ModItems.SALTED_COOKED_COD.get(), ModItems.PINK_SALTED_COOKED_COD.get(), pWriter);
+        addRecipePair(Items.COOKED_MUTTON, ModItems.SALTED_COOKED_MUTTON.get(), ModItems.PINK_SALTED_COOKED_MUTTON.get(), pWriter);
+        addRecipePair(Items.COOKED_PORKCHOP, ModItems.SALTED_COOKED_PORKCHOP.get(), ModItems.PINK_SALTED_COOKED_PORKCHOP.get(), pWriter);
+        addRecipePair(Items.COOKED_RABBIT, ModItems.SALTED_COOKED_RABBIT.get(), ModItems.PINK_SALTED_COOKED_RABBIT.get(), pWriter);
+        addRecipePair(Items.COOKED_SALMON, ModItems.SALTED_COOKED_SALMON.get(), ModItems.PINK_SALTED_COOKED_SALMON.get(), pWriter);
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.SALT_WATER_BUCKET.get())
                 .requires(ModItems.SALT.get())
@@ -386,6 +429,22 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('S', ModItems.ANCIENT_STAFF_FRAGMENT.get())
                 .unlockedBy(getHasName(ModItems.ANCIENT_STAFF_FRAGMENT.get()), has(ModItems.ANCIENT_STAFF_FRAGMENT.get()))
                 .save(pWriter);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.SALTED_CARROT_ON_A_STICK.get())
+                .pattern("# ")
+                .pattern(" X")
+                .define('#', Items.FISHING_ROD)
+                .define('X', ModItems.SALTED_CARROT.get())
+                .unlockedBy(getHasName(Items.FISHING_ROD), has(Items.FISHING_ROD))
+                .unlockedBy(getHasName(ModItems.SALTED_CARROT.get()), has(ModItems.SALTED_CARROT.get()))
+                .save(pWriter);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.PINK_SALTED_CARROT_ON_A_STICK.get())
+                .pattern("# ")
+                .pattern(" X")
+                .define('#', Items.FISHING_ROD)
+                .define('X', ModItems.PINK_SALTED_CARROT.get())
+                .unlockedBy(getHasName(Items.FISHING_ROD), has(Items.FISHING_ROD))
+                .unlockedBy(getHasName(ModItems.PINK_SALTED_CARROT.get()), has(ModItems.PINK_SALTED_CARROT.get()))
+                .save(pWriter);
 
     }
 
@@ -395,5 +454,44 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     protected static void oreBlasting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
         oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
+    }
+
+    private void addRecipePair(ItemLike baseItem, Item saltedResult, Item pinkSaltedResult,Consumer<FinishedRecipe> pWriter) {
+        // Regular salt recipe
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, saltedResult)
+                .requires(ModItems.SALT.get())
+                .requires(baseItem)
+                .unlockedBy(getHasName(ModItems.SALT.get()), has(ModItems.SALT.get()))
+                .unlockedBy(getHasName(baseItem), has(baseItem))
+                .save(pWriter);
+
+        // Pink salt recipe
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, pinkSaltedResult)
+                .requires(ModItems.PINK_SALT.get())
+                .requires(baseItem)
+                .unlockedBy(getHasName(ModItems.PINK_SALT.get()), has(ModItems.PINK_SALT.get()))
+                .unlockedBy(getHasName(baseItem), has(baseItem))
+                .save(pWriter);
+    }
+
+    // Helper methods
+    private String name(Item item) {
+        return ForgeRegistries.ITEMS.getKey(item).getPath();
+    }
+
+    private String getItemName(Item item) {
+        return "has_" + ForgeRegistries.ITEMS.getKey(item).getPath();
+    }
+
+    private void addSmeltingPair(Item saltedRaw, Item saltedCooked, Item pinkSaltedRaw, Item pinkSaltedCooked,Consumer<FinishedRecipe> pWriter) {
+        // Regular salt smelting
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(saltedRaw), RecipeCategory.FOOD, saltedCooked, 0.35F, 200)
+                .unlockedBy("has_" + getItemName(saltedRaw), has(saltedRaw))
+                .save(pWriter, name(saltedCooked) + "_from_smelting_" + name(saltedRaw));
+
+        // Pink salt smelting
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(pinkSaltedRaw), RecipeCategory.FOOD, pinkSaltedCooked, 0.35F, 200)
+                .unlockedBy("has_" + getItemName(pinkSaltedRaw), has(pinkSaltedRaw))
+                .save(pWriter, name(pinkSaltedCooked) + "_from_smelting_" + name(pinkSaltedRaw));
     }
 }
