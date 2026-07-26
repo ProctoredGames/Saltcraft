25a8153ebe77b12564df0c57e890939a23f65051
Proctored Games <proctoredgames@hotmail.com>
2026-07-26 13:03:24 -0700

    1.6.1 release

--- src/main/java/net/proctoredgames/saltcraft/datagen/ModBlockTagProvider.java
+++ src/main/java/net/proctoredgames/saltcraft/datagen/ModBlockTagProvider.java
@@ -9,7 +9,6 @@ import net.proctoredgames.saltcraft.util.ModTags;
 
 import java.util.concurrent.CompletableFuture;
 
-
 public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
     public ModBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
         super(output, registriesFuture);
